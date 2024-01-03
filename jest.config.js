d8c54d9bf19f130c5507cb9f548f32e8c9f8822a
Andrea Coronese <sixpounder@protonmail.com>
2024-01-03 11:26:09 +0100

    Fix jest run with commonjs lodash (dev only), Add some docs

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
     transform: { "^.+\\.ts?$": "ts-jest" },
     testEnvironment: "node",
     testRegex: "/tests/.*\\.(test|spec)?\\.(ts|tsx)$",
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    moduleNameMapper: {
+        "^lodash-es$": "lodash"
+    }
 };
--- src/filter/builtin/index.ts
+++ src/filter/builtin/index.ts
@@ -1,6 +1,11 @@
 import { get, has, isString, isUndefined } from "lodash-es";
 import { isElement } from "../../inspection";
 
+/**
+ * Returns a predicate that filters based on the `tagName` property of an object
+ * @param name - the name to match the `tagName` against
+ * @returns - The predicate
+ */
 export const tagName = (name: string | string[]): (el: any) => boolean => {
     return (el) => {
         let tname: string;
@@ -23,6 +28,11 @@ export const tagName = (name: string | string[]): (el: any) => boolean => {
     }
 }
 
+/**
+ * Returns a predicate that filter on an `Element` having a specific class in its `classlist`
+ * @param klass - the class to find
+ * @returns - The predicate
+ */
 export const hasClass = (klass: string | string[]): (el: any) => boolean => {
     const cls = Array.isArray(klass) ? klass : [klass];
     return (el) => {
@@ -38,6 +48,14 @@ export const hasClass = (klass: string | string[]): (el: any) => boolean => {
     }
 }
 
+/**
+ * Returns a predicate that filter on an `Element` having a specific attribute in its attribute list. If a value
+ * for that attribute is specified the predicate will also require the attribute's value to match that input value,
+ * otherwise the `Element` having that attribute is sufficient.
+ * @param attr - The attribute to search for
+ * @param val - The attribute value to match against
+ * @returns - The predicate
+ */
 export const attr = (attr: string, val?: string): (el: any) => boolean => {
     return (el) => {
         if (!isElement(el)) {
@@ -50,6 +68,24 @@ export const attr = (attr: string, val?: string): (el: any) => boolean => {
     };
 }
 
+/**
+ * A predicate returning true if an object has a property with a given `name` and, optionally, having a specific `value`
+ * @param name - The name of the property
+ * @param val - The value of the property (optional)
+ * @returns - The predicate
+ * 
+ * # Example
+ * 
+ * ```typescript
+ * select().from({ a: 1, b: 2}).where(prop("a")).run() // [{ a: 1, a: 2 }]
+ * 
+ * select().from({ a: 1, b: 2}).where(prop("a", 1)).run() // [{ a: 1, a: 2 }]
+ * 
+ * select().from({ a: 1, b: 2}).where(prop("a", "hello")).run() // []
+ * 
+ * select().from({ a: 1, b: 2}).where(prop("c")).run() // []
+ * ```
+ */
 export const prop = (name: string, val?: string): (el: any) => boolean => {
     return (el) => {
         if (isUndefined(val)) {
