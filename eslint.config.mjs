4dc977572659314bb22cff4dc52978fad41a495c
Andrea Coronese <sixpounder@protonmail.com>
2026-08-16 15:45:47 +0200

    chore: move custom eslint to build folder

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
-import { jql } from "./eslint/jql.js";
+import { jql } from "./build/eslint/jql.js";
 
 export default defineConfig([
   globalIgnores(["node_modules/**/*", "dist/**/*", "build/**/*", ".husky/", ".github/**/*"]),
